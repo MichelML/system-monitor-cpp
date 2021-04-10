2bea752ce7b406a91d70bf6cabd35a0ad890e02e
michelml <michmoreau.l@gmail.com>
2021-04-10 12:46:46 -0400

    complete kernel and os names returned

--- src/system.cpp
+++ src/system.cpp
@@ -7,6 +7,7 @@
 #include <string>
 #include <vector>
 
+#include "linux_parser.h"
 #include "process.h"
 #include "processor.h"
 
@@ -15,20 +16,22 @@ using std::size_t;
 using std::string;
 using std::vector;
 
+System::System() {
+  os_ = LinuxParser::OperatingSystem();
+  kernel_ = LinuxParser::Kernel();
+}
 // TODO: Return the system's CPU
 Processor& System::Cpu() { return cpu_; }
 
 // TODO: Return a container composed of the system's processes
 vector<Process>& System::Processes() { return processes_; }
 
-// TODO: Return the system's kernel identifier (string)
-std::string System::Kernel() { return string(); }
+std::string System::Kernel() { return kernel_; }
 
 // TODO: Return the system's memory utilization
 float System::MemoryUtilization() { return 0.0; }
 
-// TODO: Return the operating system name
-std::string System::OperatingSystem() { return string(); }
+std::string System::OperatingSystem() { return os_; }
 
 // TODO: Return the number of processes actively running on the system
 int System::RunningProcesses() { return 0; }
